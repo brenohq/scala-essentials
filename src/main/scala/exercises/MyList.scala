de431704b21ebeec2edd423de3bc133f8f0e91d2
brenohq <brenosc2@hotmail.com>
2024-09-20 10:35:14 -0300

    sort impl

--- src/main/scala/exercises/MyList.scala
+++ src/main/scala/exercises/MyList.scala
@@ -27,6 +27,7 @@ abstract class MyList[+A] {
 
   // more hofs
   def foreach(f: A => Unit): Unit
+  def sort(compare: (A, A) => Int): MyList[A]
 }
 
 object Empty extends MyList[Nothing] {
@@ -43,6 +44,7 @@ object Empty extends MyList[Nothing] {
   override def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
 
   override def foreach(f: Nothing => Unit): Unit = ()
+  override def sort(compare: (Nothing, Nothing) => Int): MyList[Nothing] = Empty
 }
 
 class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
@@ -70,6 +72,16 @@ class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
     f(h)
     t.foreach(f)
   }
+
+  override def sort(compare: (A, A) => Int): MyList[A] = {
+    def insert(x: A, sortedList: MyList[A]): MyList[A] =
+      if (sortedList.isEmpty) new Cons(x, Empty)
+      else if (compare(x, sortedList.head) <= 0) new Cons(x, sortedList)
+      else new Cons(sortedList.head, insert(x, sortedList.tail))
+
+    val sortedTail = t.sort(compare)
+    insert(h, sortedTail)
+  }
 }
 
 object ListTest extends App {
@@ -91,4 +103,6 @@ object ListTest extends App {
   println(cloneListOfIntegers == listOfIntegers)
 
   listOfIntegers.foreach(println)
+
+  println(listOfIntegers.sort((x, y) => y - x))
 }
