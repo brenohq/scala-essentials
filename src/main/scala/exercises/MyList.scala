2625af8400968dd04f0d9f9eff6e91a43ec2c20e
brenohq <brenosc2@hotmail.com>
2024-09-20 10:08:53 -0300

    updating MyList with type annotations

--- src/main/scala/exercises/MyList.scala
+++ src/main/scala/exercises/MyList.scala
@@ -1,6 +1,6 @@
 package exercises
 
-abstract class MyList {
+abstract class MyList[+A] {
 
   /*
   head = first element of the list
@@ -10,43 +10,76 @@ abstract class MyList {
   toString = a string representation of the list
    */
 
-  def head: Int
-  def tail: MyList
+  def head: A
+  def tail: MyList[A]
   def isEmpty: Boolean
-  def add(x: Int): MyList
+  def add[B >: A](x: B): MyList[B]
   def printElements: String
   override def toString: String = "[" + printElements + "]"
+
+  // higher order functions
+  def map[B](transformer: A => B): MyList[B]
+  def flatMap[B](transformer: A => MyList[B]): MyList[B]
+  def filter(predicate: A => Boolean): MyList[A]
+
+  // concatenation
+  def ++[B >: A](list: MyList[B]): MyList[B]
 }
 
-object Empty extends MyList {
-  override def head: Int = throw new NoSuchElementException()
-  override def tail: MyList = throw new NoSuchElementException()
+object Empty extends MyList[Nothing] {
+  override def head: Nothing = throw new NoSuchElementException
+  override def tail: MyList[Nothing] = throw new NoSuchElementException
   override def isEmpty: Boolean = true
-  override def add(x: Int): MyList = new Cons(x, Empty)
+  override def add[B >: Nothing](x: B): MyList[B] = new Cons(x, Empty)
   override def printElements: String = ""
+
+  override def map[B](transformer: Nothing => B): MyList[B] = Empty
+  override def flatMap[B](transformer: Nothing => MyList[B]): MyList[B] = Empty
+  override def filter(predicate: Nothing => Boolean): MyList[Nothing] = Empty
+
+  override def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
 }
 
-class Cons(h: Int, t: MyList) extends MyList {
-  override def head: Int = h
-  override def tail: MyList = t
+class Cons[+A](h: A, t: MyList[A]) extends MyList[A] {
+  override def head: A = h
+  override def tail: MyList[A] = t
   override def isEmpty: Boolean = false
-  override def add(x: Int): MyList = new Cons(x, this)
+  override def add[B >: A](x: B): MyList[B] = new Cons(x, this)
   override def printElements: String =
-    if (t.isEmpty) "" + h
-    else h + " " + t.printElements
+    if (t.isEmpty) s"$h"
+    else s"$h ${t.printElements}"
+
+  override def map[B](transformer: A => B): MyList[B] =
+    new Cons(transformer(h), t.map(transformer))
+
+  override def flatMap[B](transformer: A => MyList[B]): MyList[B] =
+    transformer(h) ++ t.flatMap(transformer)
+
+  override def filter(predicate: A => Boolean): MyList[A] =
+    if (predicate(h)) new Cons(h, t.filter(predicate))
+    else t.filter(predicate)
+
+  override def ++[B >: A](list: MyList[B]): MyList[B] = new Cons(h, t ++ list)
 }
 
 object ListTest extends App {
-  val list = new Cons(1, Empty)
+  val list = new Cons(1, new Cons(2, new Cons(3, Empty)))
   println(list.head)
+  println(list.tail.head)
+  println(list.add(4).head)
+  println(list.isEmpty)
 
-  val anotherList = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  println(list.toString)
 
-  println(anotherList.tail.head)
+  val listOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  val anotherListOfIntegers: MyList[Int] = new Cons(4, new Cons(5, Empty))
+  val listOfStrings: MyList[String] = new Cons("Hello", new Cons("Scala", Empty))
 
-  println(anotherList.add(4).head)
+  println(listOfIntegers.toString)
+  println(listOfStrings.toString)
 
-  println(anotherList.isEmpty)
-
-  println(anotherList.toString)
+  println(listOfIntegers.map(_ * 2).toString)
+  println(listOfIntegers.filter(_ % 2 == 0).toString)
+  println((listOfIntegers ++ anotherListOfIntegers).toString)
+  println(listOfIntegers.flatMap(elem => new Cons(elem, new Cons(elem + 1, Empty))).toString)
 }
